66308994d0e23d02437e3ea0f2ef673637464bdd
huongtvFX <117398702+huongtvFX@users.noreply.github.com>
2023-03-08 18:24:53 +0700

    Update VanBan

--- src/main/java/Cau3/VanBan.java
+++ src/main/java/Cau3/VanBan.java
@@ -1,54 +1,60 @@
 package Cau3;
 
 public class VanBan {
-    private String chuoiVanBan;
+    // thuộc tính là một chuỗi kí tự
+    protected String str;
 
-    public VanBan() {
-    }
-
-    public VanBan(String chuoiVanBan) {
-        this.chuoiVanBan = chuoiVanBan;
-    }
-    public String getChuoiVanBan() {
-        return chuoiVanBan;
+    // Hàm khởi tạo không có tham số
+    VanBan() {
     }
 
-    public void setChuoiVanBan(String chuoiVanBan) {
-        this.chuoiVanBan = chuoiVanBan;
-    }
-    public int wordCount() {
-        String[] words = chuoiVanBan.split("\\s+");
-        return words.length;
-    }
-    public String toUpper() {
-        this.chuoiVanBan = this.chuoiVanBan.toUpperCase();
-        return chuoiVanBan;
-    }
-    public String hoaThanhThuong() {
-        this.chuoiVanBan = this.chuoiVanBan.toLowerCase();
-        return chuoiVanBan;
+    // Hàm khởi tạo có tham số
+    VanBan(String st) {
+        str = st;
     }
 
-    public String vietHoaDau() {
-        String[] words = chuoiVanBan.split("\\s+");
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < words.length; i++) {
-            String word = words[i];
-            if (word.length() > 0) {
-                char firstLetter = word.charAt(0);
-                String restOfWord = word.substring(1);
-                String capitalizedWord = Character.toUpperCase(firstLetter) + restOfWord;
-                result.append(capitalizedWord);
-            }
-            if (i < words.length - 1) result.append(" ");
+    // Đếm số từ của văn bản
+    static int dem(VanBan vb) {
+        int d;
+        vb.str = ChuanHoa(vb);
+        if (vb.str.charAt(0) == ' ') d = 0;
+        else d = 1;
+        for (int i = 0; i < vb.str.length(); i++)
+            if (vb.str.charAt(i) == ' ' && vb.str.charAt(i + 1) != ' ') d++;
+        return d;
+    }
+
+    // chuẩn hóa một xâu theo tiêu chuẩn
+    static String ChuanHoa(VanBan vb) {
+        StringBuffer vbn = new StringBuffer(vb.str);
+        while (vbn.charAt(0) == '\0') {
+            vbn = vbn.delete(0, 1);
         }
-        this.chuoiVanBan = result.toString();
-        return chuoiVanBan;
+        while (vbn.charAt(vbn.length() - 1) == '\0') ;
+//        vbn = vbn.delete(vbn.length() - 1, vbn.length());
+        for (int i = 1; i < vbn.length(); i++)
+            while (vbn.charAt(i) == '\0' && vbn.charAt(i + 1) == '\0') vbn = vbn.delete(i, i + 1);
+        return (vbn.toString());
     }
-    @Override
-    public String toString() {
-        return "VanBan{" +
-                "chuoiVanBan='" + chuoiVanBan + '\'' +
-                '}';
+
+    // chuyển thành viết hoa
+    static String ConvertUpperCase(VanBan vanban) {
+        return vanban.str.toUpperCase();
+    }
+
+    // chuyển thành viết thường
+    static String ConvertLowerCase(VanBan vanBan) {
+        return vanBan.str.toLowerCase();
+    }
+
+    // viết hoa chữ cái đầu mỗi từ
+    static String ConvertCapitalize(VanBan vanBan) {
+        // cắt chuỗi thành từng từ dựa vào khoảng trắng
+        String[] arr = vanBan.str.split(" ");
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(Character.toUpperCase(arr[i].charAt(0))).append(arr[i].substring(1)).append(" ");
+        }
+        return sb.toString().trim();
     }
 }
